455e1a1273d0e88719b4783f64951b0193a0ca0b
stockw <courtney.wilkins05@gmail.com>
2023-06-01 15:03:31 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// let x=15;
+// console.log(x);
+
+//let a=7
+//console.log(a);
+
+//let names=["jim", "james", "paul", "tyrone"];
+//console.log(names);
+
+let seed1="carrots";
+console.log(seed1)
+
+let vegetables=20;
+console.log(vegetables);
+
+// let x = 28;
+// console.log(x);
+// I would like x to equal 28 for some random reason
+
+let x=12;
+let a=78;
+
+if (x ===12 && a ===78) {
+    let sum=x+a;
+    console.log(sum);
+}
