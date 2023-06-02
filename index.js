c6274611486820a556e8d5f904674821ddf72d3e
stockw <courtney.wilkins05@gmail.com>
2023-06-02 14:53:11 -0400

    second commit/if statements

--- index.js
+++ index.js
@@ -7,20 +7,70 @@
 //let names=["jim", "james", "paul", "tyrone"];
 //console.log(names);
 
-let seed1="carrots";
-console.log(seed1)
+// let seed1="carrots";
+// console.log(seed1)
 
-let vegetables=20;
-console.log(vegetables);
+// let vegetables=20;
+// console.log(vegetables);
 
 // let x = 28;
 // console.log(x);
 // I would like x to equal 28 for some random reason
 
-let x=12;
-let a=78;
+// let x=12;
+// let a=78;
 
-if (x ===12 && a ===78) {
-    let sum=x+a;
-    console.log(sum);
+// if (x ===12 && a ===78) {
+//     let sum=x+a;
+//     console.log(sum);
+// }
+
+
+// let age = 18;
+// if (age >18) {
+//     //do this
+//     console.log("access granted");
+// }
+// else{
+//     //do this
+//     console.log("access denied");
+// }
+
+// let wood=19;
+// if (wood >= 40) {
+//     //we have enough for a treehouse
+//      console.log("you can build a treehouse")
+// }
+// else if (wood >=20) {
+//     //to bad so sad
+//      console.log("build stool");
+// }
+// else{
+//    console.log("you are s.o.l");
+// }
+
+// let username = 'admin';
+// let password = 'Pass614';
+
+// if (username ==='admin' && password ==='Pass614') {
+//     console.log("access granted");
+// } 
+// else{
+//     console.log("access denied");
+// }
+
+let num= 0;
+
+if (num > 0) {
+    if (num > 100){
+    console.log(" num is positive and greater than 100");
+} else{
+    console.log("it's positive but less than 100 ");
 }
+} else if (num < 0) {
+    console.log("number is negative");
+} else {
+    console.log("number is zero");
+}
+
+// !== is the same as saying not equal
